6c4f7733398ed638372ee76df7ec48f32761728e
Cyber Knight <cyberknight755@gmail.com>
2022-09-16 14:56:28 +0000

    raphael: Build KProfiles - Inherit KProfiles. - Address sepolicy. - Define sysfs_kprofiles as a domain. - Fix permission of nodes relevant to KProfiles at boot.
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     OtoMusicPlayer
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
