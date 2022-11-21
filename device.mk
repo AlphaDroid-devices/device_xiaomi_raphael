7eefece1d62f82bb9b0c08db6a42243c7a3dcc9b
Ronalds <7190697+kondors1995@users.noreply.github.com>
2022-11-21 14:32:52 +0000

    raphael: Add AAPT config
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ $(call inherit-product-if-exists, vendor/GcamBSG/GcamBSG-vendor.mk)
 # MiuiCamera
 $(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
