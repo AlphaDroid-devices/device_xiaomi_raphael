5383d7137c20838dd691a978fb9dd7370d452270
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    raphael: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -458,6 +458,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
