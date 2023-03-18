147585072c34671b6e089bed8359b5d96d279208
onettboots <blackcocopet@gmail.com>
2023-03-18 13:17:15 +0000

    raphael: parts: Bringback dirac audio

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -33,6 +34,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+
+	try {
+        DiracUtils.initialize(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
         PopupCameraUtils.startService(context);
     }
 }
