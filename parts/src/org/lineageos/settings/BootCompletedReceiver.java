bbe6402000c2d6f49727e2365b07b15d96220cf1
onettboots <blackcocopet@gmail.com>
2023-11-20 06:14:59 +0000

    Revert "raphael: parts: Disable dc dimming if it's not supported"
    This reverts commit 7be7cfe3b9aefa6baa94b0e585ace37a4653b215.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -46,6 +46,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
-        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+        try {
+            FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+        } catch(Exception e) {}
     }
 }
