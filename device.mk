c72defa2203e93b95d1be05ac56605c6e14c10f9
onettboots <blackcocopet@gmail.com>
2024-10-25 16:40:33 +0000

    raphael: build Otomusic
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -359,6 +359,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# OtoMusicPlayer
+PRODUCT_PACKAGES += \
+    OtoMusicPlayer
+
 # KProfiles
 PRODUCT_PACKAGES += \
     KProfiles
